cf3c3bbf7fde5c2c9d55751846b9568a45a78f28
shivk40 <shivk40@users.noreply.github.com>
2016-01-23 14:37:34 -0500

    Hello World
    Hello World

--- src/main/java/org/home/shiv/HelloGanesha.java
+++ src/main/java/org/home/shiv/HelloGanesha.java
@@ -3,8 +3,8 @@ package org.home.shiv;
 /**
  * Created by Bhuju on 1/23/2016.
  */
-public class HelloWorld {
+public class HelloGanesha {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        System.out.println("Hello Ganesha!!!");
     }
 }
